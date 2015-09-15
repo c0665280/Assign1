07c7f489eaa23bccc61de2033c769526da45bf4c
c0665280 <c0665280@mylambton.ca>
2015-09-14 23:02:57 -0400

    Update Assign1.java

--- src/assign1/Assign1.java
+++ src/assign1/Assign1.java
@@ -21,18 +21,27 @@ public class Assign1 {
     /***********************
      * Assignment 1
      * Job Posting
-     * http://www.indeed.ca/cmp/THEMESOFT/jobs/Hadoop-Developer-cb30d666fd9ad352?sjdu=vQIlM60yK_PwYat7ToXhkzk2QN2pmLXdt1fL2hqmWLqBIzoECv0bpUjQYG_UFUz9bSTwJaBGzFM4efr6vV63Og
-     * Hadoop Developer - Themesoft, Missisauga
-     * 
+     * http://autodata.applytojob.com/apply/2d17200c5f5b757277504063695b5e7848621537151c3b451e2f29210d372e081e3112/Intermediate-Java-Developer?source=INDE&sid=GxBSmkDGGgrZXnlAzWJtEHasWvmd5dzLKbD&source=INDE
+     * INTERMEDIATE JAVA DEVELOPER
+       LOCATION : LONDON, ONTARIO
+       REQUIRED EXPERIENCE : MID-LEVEL (MINIMUM)
+
+     
      * J2EE  https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
-     * 
-     * Hadoop  https://en.wikipedia.org/wiki/Apache_Hadoop
-     * 
-     * Cloudera  https://en.wikipedia.org/wiki/Cloudera
-     * 
-     * Toronto Salary 
-     * Vancouver Salary
-     * Waterloo Salary 
+     * HTML 5  https://en.wikipedia.org/wiki/HTML5
+     * JQUERY  https://en.wikipedia.org/wiki/JQuery
+     * SPRING FRAMEWORK  https://en.wikipedia.org/wiki/Spring_Framework
+     * MYBATIS  https://en.wikipedia.org/wiki/MyBatis
+     * C++  https://en.wikipedia.org/wiki/C%2B%2B
+     * C#  https://en.wikipedia.org/wiki/C_Sharp_%28programming_language%29
+     * TOMCAT  https://en.wikipedia.org/wiki/Apache_Tomcat
+     * MAVEN  https://en.wikipedia.org/wiki/Maven
+     * SQL  https://en.wikipedia.org/wiki/SQL
+     
+     
+     * Toronto Salary 75k
+     * Vancouver Salary 86k
+     * Waterloo Salary 75k
      * 
      * 
     ***********************/
